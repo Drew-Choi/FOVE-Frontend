12190d8a4c87ce505ecd51fa7bbe9a686acac0bb
sang ah <kr.sangah@gmail.com>
2023-04-07 09:48:21 +0900

    230407 실험

--- src/components/admin_components/Home_admin.jsx
+++ src/components/admin_components/Home_admin.jsx
@@ -4,7 +4,7 @@ export default function Home_admin() {
   return (
     <>
       <h1 style={{ textAlign: 'center', display: 'inline-block' }}>
-        홈입니다.
+        홈입니다. 어쩌구 저쩌구! 수정 오류 잡기!
       </h1>
     </>
   );
