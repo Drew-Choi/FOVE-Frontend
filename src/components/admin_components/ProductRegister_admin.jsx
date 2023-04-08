6969fa1f4eb711b1d657018208c4fb700b0c5b14
Drew <drc920630@gmail.com>
2023-04-09 00:38:35 +0900

    라디오 수정 완료

--- src/components/admin_components/ProductRegister_admin.jsx
+++ src/components/admin_components/ProductRegister_admin.jsx
@@ -1,8 +1,11 @@
 import React, { useRef, useState } from 'react';
 import '../../styles/productRegister_admin.scss';
 import axios from 'axios';
+import RadioGroup from './etc_elements/RadioGroup';
+import RadioEl from './etc_elements/RadioEl';
 
 export default function ProductRegister_admin() {
+  //-------
   //가격 콤마용
   const [enterNumPrice, setEnterNumPrice] = useState('');
   //재고수량 콤마용
@@ -24,11 +27,16 @@ export default function ProductRegister_admin() {
   const resultCommaRemove = (el) => {
     return Number(el.split(',').reduce((curr, acc) => curr + acc, ''));
   };
+  //-------
 
   //input 값을 받을 useRef생성
   const pd_name = useRef();
   const pd_price = useRef();
+  const pd_size = useRef();
   const pd_quantity = useRef();
+
+  //사이즈를 위한 state
+  const [sizeType, setSizeType] = useState('');
 
   //클릭이벤트시 실행될 함수 생성
   //기능: 클릭 발생하면 axios로 서버에 해당 페이지 요청을 보냄
@@ -37,15 +45,17 @@ export default function ProductRegister_admin() {
   const newProductPost = async () => {
     const pdName = pd_name.current.value;
     const pdPrice = resultCommaRemove(pd_price.current.value);
+    const pdSize = pd_size.current.value;
     const pdQuantity = resultCommaRemove(pd_quantity.current.value);
 
     //데이터 확인용 콘솔로그
     console.log(pdName);
     console.log(pdPrice);
+    console.log(pdSize);
     console.log(pdQuantity);
 
     //빈 인풋이 없는 지 체크
-    if (!pdName || !pdPrice || !pdQuantity)
+    if (!pdName || !pdPrice || !pdQuantity || !pdSize)
       return alert('모든 필수 정보를 입력해 주세요.');
     //async/await를 이용해 axios 구현
     const newPdPostData = await axios.post(
@@ -55,6 +65,7 @@ export default function ProductRegister_admin() {
       {
         name: pdName,
         price: pdPrice,
+        size: pdSize,
         quantity: pdQuantity,
       },
     );
@@ -66,11 +77,12 @@ export default function ProductRegister_admin() {
       //서버에서 res로 날라오는 내용을 로그로 띄움
       console.log(await newPdPostData);
     }
+    newPdPostData();
   };
 
   return (
     <section className="productRegister_admin">
-      <div className="register_container">
+      <form className="register_container">
         <div>
           <p>상품명</p>
           <input
@@ -95,6 +107,37 @@ export default function ProductRegister_admin() {
             }
           />
         </div>
+        <RadioGroup style={{}}>
+          사이즈 &nbsp;&nbsp;&nbsp;&nbsp;
+          <RadioEl
+            inputref={pd_size}
+            name="size"
+            value={sizeType}
+            onClick={() => setSizeType((cur) => 'S')}
+            defaultChecked
+          >
+            S
+          </RadioEl>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <RadioEl
+            inputref={pd_size}
+            name="size"
+            value={sizeType}
+            onClick={() => setSizeType((cur) => 'M')}
+          >
+            M
+          </RadioEl>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <RadioEl
+            inputref={pd_size}
+            name="size"
+            value={sizeType}
+            onClick={() => setSizeType((cur) => 'L')}
+          >
+            L
+          </RadioEl>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </RadioGroup>
         <div>
           <p>수량&nbsp;&nbsp;&nbsp;</p>
           <input
@@ -111,9 +154,33 @@ export default function ProductRegister_admin() {
             }
           />
         </div>
+        <div>
+          <p>상품 메인 이미지</p>
+          <input type="file" name="img" />
+        </div>
+        <div>
+          <p>상품 서브 이미지1</p>
+          <input type="file" name="img" />
+        </div>
+        <div>
+          <p>상품 서브 이미지2</p>
+          <input type="file" name="img" />
+        </div>
+        <div>
+          <p>상품 서브 이미지3</p>
+          <input type="file" name="img" />
+        </div>
+        <div>
+          <p>상품 서브 이미지4</p>
+          <input type="file" name="img" />
+        </div>
+        <div>
+          <p>상품 서브 이미지5</p>
+          <input type="file" name="img" />
+        </div>
         {/* 클릭시 axios각 작동할 수 있게 위에 만든 함수를 넣어준다. */}
         <button onClick={newProductPost}>등록</button>
-      </div>
+      </form>
     </section>
   );
 }
