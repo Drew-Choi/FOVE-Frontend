52c84eb95ccabf7a3dc64e209c559fbd1a30100f
Drew <drc920630@gmail.com>
2023-04-11 20:08:46 +0900

    feat[#26] : 인트로 동영상들어가는 자리 만듦 푸터 복원 완료

---
+++ src/components_pages/client_components/Intro_movie_clinet.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/intro_Movie_client.scss';
+
+export default function Intro_movie_client() {
+  return (
+    <section className="intor_moive">
+      <h1 style={{ fontSize: '100px', position: 'absolute', top: '100px' }}>
+        영상이 들어갈 예정임
+      </h1>
+    </section>
+  );
+}
