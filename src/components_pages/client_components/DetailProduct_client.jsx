eb28d541f3110cad3a1e6487806da96b4c772ab3
Drew <drc920630@gmail.com>
2023-04-12 00:58:43 +0900

    feat#47 : get요청 완료 디자인은 내일~~

--- src/components_pages/client_components/DetailProduct_client.jsx
+++ src/components_pages/client_components/DetailProduct_client.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../../styles/detailProduct_client.scss';
+import Detail_Product_Layoutclient from './Detail_Product_Layoutclient ';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function DetailProduct_client() {
-  return <div>aaaa</div>;
+  const { id } = useParams();
+  const [productData, setProductData] = useState();
+
+  useEffect(() => {
+    getSelectProduct();
+  }, []);
+
+  const getSelectProduct = async () => {
+    const selectData = await axios.get(
+      `http://localhost:4000/store/productId/${id}`,
+    );
+    if (selectData.status === 200) {
+      await setProductData(selectData.data);
+      return selectData.data;
+    } else {
+      return selectData.data;
+    }
+  };
+
+  console.log(id);
+  console.log(productData);
+
+  return (
+    <section className="pd_detail">
+      {productData && (
+        <Detail_Product_Layoutclient imgFileName={productData[0].img[0]} />
+      )}
+    </section>
+  );
 }
