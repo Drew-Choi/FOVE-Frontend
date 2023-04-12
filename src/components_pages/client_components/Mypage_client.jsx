1e1ccf8e388526f5919d942adcd7a7707d1cff41
sang ah <kr.sangah@gmail.com>
2023-04-12 17:57:05 +0900

    230412 로그인 후 mypage 작업중

--- src/components_pages/client_components/Mypage_client.jsx
+++ src/components_pages/client_components/Mypage_client.jsx
@@ -6,12 +6,12 @@ export default function Mypage_client() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="inner">
+      {/* ACCOUNT 제목 위치 */}
+      <div className="titleArea">
+        <h2 className="subtitle">ACCOUNT</h2>
+      </div>
+      <div className="account_inner">
         <div className="account_wrap">
-          {/* ACCOUNT 제목 위치 */}
-          <div className="titleArea">
-            <h2 className="subtitle">ACCOUNT</h2>
-          </div>
           {/* 회원 등급 박스 */}
           <div className="information">
             <p className="thumbnail">
@@ -22,18 +22,15 @@ export default function Mypage_client() {
                 저희 쇼핑몰을 이용해주셔서 감사합니다.
                 <br />
                 <span className="name">김영호</span> 님은
-                <img
-                  className="namegrade"
-                  src="/images/standard.jpg"
-                ></img>{' '}
+                <span className="namegrade"> STANDARD </span>
                 회원이십니다.
                 <br />
                 <span className="price"> KRW 10,000 이상</span> 구매시
-                <span className="percent">5%</span>을 추가할인 받으실 수
+                <span className="percent"> 5% </span>을 추가할인 받으실 수
                 있습니다. (최대 KRW 9,999,999)
                 <br />
                 <span className="price"> KRW 10,000 이상</span> 구매시
-                <span className="percent">5%</span>을 추가적립 받으실 수
+                <span className="percent"> 5% </span>을 추가적립 받으실 수
                 있습니다. (최대 KRW 9,999,999)
               </span>
             </div>
@@ -42,9 +39,11 @@ export default function Mypage_client() {
           <div className="orderstate">
             <div className="orderstate_box">
               <div className="title">
-                <h3>
-                  나의 주문처리 현황 <span>(최근 3개월 기준)</span>
-                </h3>
+                <div className="title_order">
+                  <h3>
+                    나의 주문처리 현황 <span>(최근 3개월 기준)</span>
+                  </h3>
+                </div>
               </div>
               <div className="state">
                 <div className="orderbox">
