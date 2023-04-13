5b393aa9f378e9f202b7a7654cddb5e3bfa05e8a
Drew <drc920630@gmail.com>
2023-04-13 13:30:14 +0900

    feat[#62]: 카트 창에 데이터 뿌려주기 성공 플러스 작업 해야함

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './modules/user';
 import cart from './modules/cart';
+import cartmodal from './modules/cartmodal';
 
 export default combineReducers({
   cart,
   user,
+  cartmodal,
 });
