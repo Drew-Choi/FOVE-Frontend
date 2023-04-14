e1b515552fa319514b156feac605fb6900096f9a
sang ah <kr.sangah@gmail.com>
2023-04-14 13:12:51 +0900

    230414

--- src/components_pages/client_components/Add.jsx
+++ src/components_pages/client_components/Add.jsx
@@ -26,34 +26,39 @@ const Add = () => {
   };
 
   return (
-    <div className="adress_wrap">
-      <input
-        type="text"
-        value={addressData.zonecode}
-        placeholder="주소"
-        style={{ fontSize: '12px' }}
-      />
-      <button onClick={handle.clickButton}>우편번호</button>
-      {openPostcode && (
-        <DaumPostcode
-          onComplete={handle.selectAddress} // 값을 선택할 경우 실행되는 이벤트
-          autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
-          defaultQuery="판교역로 235" // 팝업을 열때 기본적으로 입력되는 검색어
+    <div className="address_mom_wrap">
+      <div className="adress_wrap">
+        <input
+          className="postcode_input"
+          type="text"
+          value={addressData.zonecode}
+          placeholder="주소"
+          style={{ fontSize: '12px' }}
         />
-      )}
-      <input
-        type="text"
-        value={addressData.address}
-        placeholder="기본주소"
-        style={{ fontSize: '12px' }}
-      />
-      <input
-        type="text"
-        value={addressData.buildingName}
-        placeholder="나머지주소 (선택입력)"
-        onChange={handleChange}
-        style={{ fontSize: '12px' }}
-      />
+        <button className="postcode_btn" onClick={handle.clickButton}>
+          우편번호
+        </button>
+        {openPostcode && (
+          <DaumPostcode
+            onComplete={handle.selectAddress} // 값을 선택할 경우 실행되는 이벤트
+            autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
+            defaultQuery="판교역로 235" // 팝업을 열때 기본적으로 입력되는 검색어
+          />
+        )}
+        <input
+          type="text"
+          value={addressData.address}
+          placeholder="기본주소"
+          style={{ fontSize: '12px' }}
+        />
+        <input
+          type="text"
+          value={addressData.buildingName}
+          placeholder="나머지주소 (선택입력)"
+          onChange={handleChange}
+          style={{ fontSize: '12px' }}
+        />
+      </div>
     </div>
   );
 };
