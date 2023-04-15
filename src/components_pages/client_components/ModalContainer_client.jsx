a36e67262f3c63a9bc6daaaa990cf14778c09212
sang ah <kr.sangah@gmail.com>
2023-04-16 01:04:11 +0900

    feat[#96] : fitsize 모달창 추가

--- src/components_pages/client_components/ModalContainer_client.jsx
+++ src/components_pages/client_components/ModalContainer_client.jsx
@@ -29,23 +29,27 @@ const ModalView = styled.div.attrs((props) => ({
   role: 'dialog',
 }))`
   display: flex;
-  gap: 330px;
-  width: 40rem;
-  /* height: auto; */
-  /* height: 30rem; */
-  padding: 1.5rem 2rem 20rem;
+  position: absolute;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 3rem;
+  padding: 0 3rem;
   background-color: white;
-  border: 1px solid black;
+  border: 2px solid black;
   justify-content: center;
   color: #121924;
   font-size: 30px;
+  margin-bottom: 60px;
 
   .sizing {
-    display: flex;
-    gap: 330px;
+    justify-content: space-between;
     font-size: 14px;
     font-weight: bold;
     color: black;
+    padding-top: 30px;
+  }
+
+  span {
+    margin-right: 200px;
   }
 
   button {
@@ -53,6 +57,84 @@ const ModalView = styled.div.attrs((props) => ({
     text-decoration: underline;
     border: none;
     font-weight: bold;
+  }
+
+  .middle_line {
+    position: fixed;
+    width: 432.66px;
+    height: 53px;
+    border-bottom: 1.5px solid darkgray;
+    z-index: 1;
+  }
+
+  .size_os_wrap {
+    display: grid;
+    grid-template-columns: 200px 200px;
+    justify-content: flex-end;
+    border-top: 1.5px solid darkgray;
+    border-bottom: 1.5px solid darkgray;
+    margin-bottom: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    gap: 20px;
+
+    .size_os1 {
+      position: relative;
+
+      /* Depth */
+      .box2_os {
+        position: absolute;
+        top: 46px;
+        text-align: center;
+        font-weight: 300;
+      }
+    }
+
+    .size_os2 {
+      display: grid;
+      gap: 20px;
+      box-sizing: border-box;
+      margin-left: 90px;
+
+      /* OS */
+      .box3_os {
+        position: relative;
+        bottom: 4px;
+        font-weight: 300;
+      }
+
+      /* 16 */
+      .box4_os {
+        position: relative;
+        top: 5px;
+        font-weight: 300;
+      }
+    }
+  }
+
+  .cen_inc {
+    display: flex;
+    justify-content: flex-end;
+    padding-bottom: 22px;
+    gap: 10px;
+    font-weight: bold;
+
+    .centi {
+      font-weight: bold;
+      transition: color 0.3s ease-in-out;
+      color: black;
+    }
+
+    .inch {
+      font-weight: bold;
+      transition: color 0.3s ease-in-out;
+      color: gray;
+
+      &:hover {
+        color: black;
+        font-weight: bold;
+      }
+    }
   }
 `;
 
@@ -77,6 +159,42 @@ export default function ModalContainer_client() {
               <div className="sizing">
                 <span>PRODUCT MEASUREMENTS</span>
                 <button onClick={openModalHandler}>CLOSE</button>
+                <div className="size-detail">
+                  <div className="size_img_wrap">
+                    <img
+                      src="./images/size_beanie.svg"
+                      alt="beanie size artwork"
+                    />
+                  </div>
+                  <div className="middle_line"></div>
+                  <div className="size_os_wrap">
+                    <div className="size_os1">
+                      <div className="size_box1">
+                        <span></span>
+                      </div>
+                      <div className="size_box2">
+                        <span className="box2_os">Depth</span>
+                      </div>
+                    </div>
+                    <div className="size_os2">
+                      <div className="size_box3">
+                        <span className="box3_os">OS</span>
+                      </div>
+                      <div className="size_box4">
+                        <span className="box4_os">16</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="cen_inc">
+                    <a className="centi" href="#">
+                      CENTIMETERS
+                    </a>
+                    /
+                    <a className="inch" href="#">
+                      INCHES
+                    </a>
+                  </div>
+                </div>
               </div>
             </ModalView>
           </ModalBackdrop>
